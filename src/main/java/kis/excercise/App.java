5bca2d8c8e7c3a0429cc59244faf73101cee9ea0
howorang <howorang@gmail.com>
2018-06-17 23:06:03 +0200

    CLI

--- src/main/java/kis/excercise/App.java
+++ src/main/java/kis/excercise/App.java
@@ -1,18 +1,30 @@
 package kis.excercise;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        MathExpression expression = new MathExpression("(1.2*3)+5-(23.4+3)^3;8:13;");
-        try {
-            expression.start();
-        } catch (IncorrectInputException e) {
-            e.printStackTrace();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        System.out.println("By wyjść podaj 'q'");
+        while (!exit) {
+            System.out.println("Podaj wyrażenie: ");
+            String input = scanner.nextLine();
+            if (input.equals("q")) {
+                break;
+            }
+            MathExpression parser = new MathExpression(input);
+            try {
+                parser.start();
+                System.out.println("Poprawna");
+            } catch (IncorrectInputException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
